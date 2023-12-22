5167101045cd807eddcd764967d916e5933432e3
Prakash Yogi <prakash.y@fyle.in>
2023-12-22 23:11:08 +0530

    added demo server file

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+var logger *log.Logger = log.Default()
+
+func main() {
+
+	fmt.Println("Go server starting up...")
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		logger.Println("Health OK")
+		w.Write([]byte("OK"))
+
+		if _, err := w.Write([]byte("Hello World!")); err != nil {
+			logger.Panic(err)
+		}
+	})
+	http.HandleFunc("/hello", handleHelloWorld)
+
+	logger.Println("Go server listening on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleHelloWorld(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		logger.Println("Invalid request method for /hello")
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+	logger.Println("Handling /hello request...")
+
+	if _, err := w.Write([]byte("Hello World!")); err != nil {
+		logger.Panic(err)
+	}
+}
